4c9683f2aaa6748d578003133dd5ae7dbdbf0ae3
yiquan <yiquan1116@gmail.com>
2022-05-21 02:27:59 +0800

    Admin Class

---
+++ src/main/java/Admin.java
@@ -0,0 +1,26 @@
+public class Admin {
+    private String name, password;
+
+    public Admin(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public void getCustomer(){
+        //Display the customer table
+    }
+
+    public void getDriver(){
+        //Display the driver table
+    }
+
+    public void addDriver(){
+        //add the driver to the database
+    }
+
+    public void removeDriver(){
+        //remove the driver from the database
+    }
+
+
+}
