aff1620ae770a43e95609c8c68f586883e9b0f3a
Wong Chou Zi <92500653+CZWong1113@users.noreply.github.com>
2022-05-21 02:48:18 +0800

    Create Customer.java

---
+++ src/main/java/Customer.java
@@ -0,0 +1,85 @@
+public class Customer {
+    private String name;
+    private String email;
+    private String address;
+    private String gender;
+    private String username;
+    private String password;
+
+    public Customer(String name, String email, String address, String gender, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.gender = gender;
+        this.username = username;
+        this.password = password;
+    }
+    
+    public void register() {
+        
+    }
+    
+    public void modify() {
+        
+    }
+    
+    public void booking() {
+        
+    }
+    
+    public void pay() {
+        
+    }
+    
+    public void setCurrentLocation() {
+        
+    }
+    
+    public void setDestination() {
+        
+    }
+    
+    public void getDriver() {
+        
+    }
+    
+    public void setCapacityRequired() {
+        
+    }
+    
+    public void displayEstimatedArrivalTime() {
+        
+    }
+    
+    public void setDriverReputation() {
+        
+    }
+    
+    public void login() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
